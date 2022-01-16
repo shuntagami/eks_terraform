30090d2c454fc8386e09bd54248d16d7693f3d61
shuntagami <shun.tagami@i-nations.tokyo>
2022-01-16 14:35:12 +0900

    Add sg for ssh

--- service/aws_security_group.tf
+++ service/aws_security_group.tf
@@ -3,6 +3,13 @@ resource "aws_security_group" "instance" {
   description = "instance sg"
   vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
